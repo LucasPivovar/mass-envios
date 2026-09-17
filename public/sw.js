1df95462ac69acf0d79cca13e42668b1e81ab67d
LucasPivovar <lucascaetano956@gmail.com>
2026-09-17 12:54:35 -0300

    style: use share icon as favicon

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'metaflow-shell-v1';
+const CACHE = 'metaflow-shell-v2';
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/offline.html', '/metaflow-logo.png'])));
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/offline.html', '/metaflow-logo.png', '/favicon-share.svg'])));
 });
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('metaflow-shell-') && key !== CACHE).map(key => caches.delete(key)))));
